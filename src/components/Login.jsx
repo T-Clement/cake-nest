4e88328abc35693f53f3f4fb8978555aa5331998
Toquet Clément <toquet.clement@gmail.com>
2024-01-29 10:08:54 +0100

    login form component, username as global  state in App

---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+function Login( { onSubmit } ) {
+    const  [username, setUsername] = useState("");
+
+    
+
+    const handleChange = (e) => {
+        setUsername(e.target.value);
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Le formulaire est soumis")
+        onSubmit(username);
+        setUsername("");
+    }
+
+
+  return (
+    <div>
+        <h1>Bienvenue chez nous !</h1>
+
+        <section>
+            <h2>Connectez-vous</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='username' value={username} placeholder='Entrez votre prénom...' onChange={handleChange}/>
+                <button type='submit'>Accédez à votre espace</button>
+            </form>
+
+        </section>
+    </div>
+  )
+}
+
+export default Login
